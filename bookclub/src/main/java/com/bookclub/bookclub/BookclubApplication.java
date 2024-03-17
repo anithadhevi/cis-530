9b000c692e7daf1109f9c740c0959b9cd690e0d0
anithadhevi <143576231+anithadhevi@users.noreply.github.com>
2024-03-17 17:09:00 -0500

    Assignment 1.2
    Assignment 1.2 Project setup and repository

--- bookclub/src/main/java/com/bookclub/bookclub/BookclubApplication.java
+++ bookclub/src/main/java/com/bookclub/bookclub/BookclubApplication.java
@@ -1,13 +1,13 @@
-package com.bookclub.demo;
+package com.bookclub.bookclub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class BookclubApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(BookclubApplication.class, args);
 	}
 
 }
--- bookclub/src/test/java/com/bookclub/bookclub/BookclubApplicationTests.java
+++ bookclub/src/test/java/com/bookclub/bookclub/BookclubApplicationTests.java
@@ -1,10 +1,10 @@
-package com.bookclub.demo;
+package com.bookclub.bookclub;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class BookclubApplicationTests {
 
 	@Test
 	void contextLoads() {
