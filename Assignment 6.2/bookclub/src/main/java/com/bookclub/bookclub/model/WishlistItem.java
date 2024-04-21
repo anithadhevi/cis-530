eea85fdd386fb66163f8b698d5c42340df2ea9be
anithadhevi <143576231+anithadhevi@users.noreply.github.com>
2024-04-21 15:32:34 -0500

    Assignment 6.2
    Access Management

---
+++ Assignment 6.2/bookclub/src/main/java/com/bookclub/bookclub/model/WishlistItem.java
@@ -0,0 +1,81 @@
+package com.bookclub.bookclub.model;
+import org.springframework.data.annotation.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
+import org.springframework.data.mongodb.core.mapping.Document;;
+
+
+@Document(collection="wishlist")
+public class WishlistItem
+{
+    @NotNull
+    @NotEmpty(message="ISBN is a required field.")
+    private String isbn;
+
+    @NotNull
+    @NotEmpty(message= "Title is a required field.")
+    private String title;
+
+    @Id
+    private String Id;
+
+    // Default constructor
+    public WishlistItem()
+    {
+
+    }
+    
+    // parameterized constructor
+    public WishlistItem(String isbn, String title)
+    {
+        this.isbn = isbn;
+        this.title = title;
+    }
+    //Getter and Setter methods for isbn and title
+
+    //Getter for isbn
+    public String getIsbn()
+    {
+        return isbn;
+    }
+   
+    //Setter for isbn
+    public void setIsbn(String isbn)
+    {
+        this.isbn=isbn;
+    }
+
+    //Getter for title
+    public String getTitle()
+    {
+        return title;
+    }
+    //Setter for title
+    public void setTitle(String title)
+    {
+        this.title=title;
+    }
+
+   //Getter for Id
+   public String getId()
+   {
+    return Id;
+   }
+
+
+/*/override toString method
+@Override
+public String toString()
+{
+     return "WishlistItem("+ "isbn=' " + isbn +'\''+", title=' " +title +'\''+'}';
+}*/
+
+//override toString method
+@Override
+public String toString()
+{
+    return "WishlistItem{"+ "Id=' " + Id +'\''+", isbn= ' " + isbn + '\''+", title= ' "+title +'\''+'}';
+}
+}
+
+
